ca29535b4a5a14f7673d6e9424010606fb06e899
Wes Domon <domonwes@yahoo.com>
2020-02-09 17:22:56 -0700

    Bug fixes on print queue script

--- Delete all print queues.ps1
+++ Delete all print queues.ps1
@@ -14,8 +14,18 @@ function Get-TimeStamp {
 
 LogWrite "** STARTING Delete all print queues Script **"
 
+$dep = Get-Service spooler -DependentServices
+if (($dep.Count) -ge 1) {
+    $isdep = $true
+    LogWrite "$($dep.Count) services are dependent on the spooler service. Stopping those services."
+    foreach ($service in $dep) {
+        Stop-Service -Name $service.Name -Force
+    }
+}
+
 try {
     Stop-Service spooler -Force -ErrorAction Stop
+    LogWrite "Spooler service successfully stopped"
 } catch {
         LogWrite "Unable to stop spooler service. Error message is:"
         LogWrite $_.Exception.Message
@@ -27,6 +37,18 @@ try {
 } catch {
         LogWrite "Unable to delete all print jobs. Error message is:"
         LogWrite $_.Exception.Message
+}
+if ($isdep) {
+    try {
+        foreach ($service in $dep) {
+            Start-Service -Name $service.Name -ErrorAction Stop
+        }
+    } catch {
+        LogWrite "Unable to start dependent service $($service.Name). Please manually start the following dependent services, then start the spooler service:"
+        foreach ($service in $dep) {
+            LogWrite "$($service.Name)"
+        }
+    }
 }
 try {
     Start-Service spooler -ErrorAction Stop
