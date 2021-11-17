b86202e62624f1208bca219a541f6a334c7c4e96
Wes Domon <domonwes@yahoo.com>
2021-11-16 22:14:43 -0700

    Update Delete IIS logs older than 30 days.ps1

--- Delete IIS logs older than 30 days.ps1
+++ Delete IIS logs older than 30 days.ps1
@@ -14,11 +14,47 @@
 
 LogWrite "** STARTING Clear IIS Logs Script **"
 
-$w3svc = Get-ChildItem -Path "C:\inetpub\logs\LogFiles"
+    $iis_servers = @(
+        "server1"
+        "server2"
+        "server3"
+    )
+    
+    foreach ($iis_server in $iis_servers) {
+try {
+    $local30 = Invoke-Command -ComputerName $iis_server -ErrorAction Stop -ScriptBlock {
+        $iislocalpath = "C:\inetpub\logs\LogFiles"
+        if (Test-Path -Path "$iislocalpath") {
+        $over30 = Get-ChildItem -Path "$iislocalpath" -Recurse | Where-Object {($_.LastWriteTime -lt (Get-Date).AddDays(-30))}
+        $over30.count
+        }
+    }
+} catch {
+    $ErrorMessage = $_.Exception.Message
+    LogWrite "IIS Log cleanup failed on $iis_server with error:"
+    LogWrite $ErrorMessage
+}
 
-foreach ($w3 in $w3svc) {
-    LogWrite "Deleting files older than 30 days in $w3"
-    Forfiles.exe -p "C:\inetpub\logs\LogFiles\$w3" -m *.log -d -30 -c "Cmd.exe /C del @path"
+    if ($local30) -gt 0) {
+        try {
+            LogWrite "Deleting $local30 files older than 30 days on $iis_server."
+            Invoke-Command -ComputerName $iis_server -Credential $srv_cred -ErrorAction Stop -ScriptBlock {
+                $iislocalpath = "C:\inetpub\logs\LogFiles"
+                Get-ChildItem -Path "$iislocalpath" -Recurse | Where-Object {($_.LastWriteTime -lt (Get-Date).AddDays(-30))} | Remove-Item -Force -Recurse -Confirm:$false
+            }
+        } catch {
+            $ErrorMessage = $_.Exception.Message
+            LogWrite "IIS Log cleanup failed on $iis_server with error:"
+            LogWrite $ErrorMessage
+        }
+
+    }
+}
+
+} catch {
+    $ErrorMessage = $_.Exception.Message
+    LogWrite "IIS Log cleanup failed with error:"
+    LogWrite $ErrorMessage
 }
 
 LogWrite "** ENDING Clear IIS Logs Script **"
